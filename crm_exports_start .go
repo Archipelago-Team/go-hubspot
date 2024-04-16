c9ca4ce8ff89d6beed8941f40a050dde24674b9d
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2024-04-16 17:03:10 +0200

    omitempty fields

--- crm_exports_start .go
+++ crm_exports_start .go
@@ -1,29 +1,30 @@
 package hubspot
 
 type CrmExportConfig struct {
-	ExportType             string                 `json:"exportType"`
-	Format                 string                 `json:"format"`
-	ExportName             string                 `json:"exportName"`
-	Language               string                 `json:"language"`
-	ObjectType             string                 `json:"objectType"`
-	AssociatedObjectType   string                 `json:"associatedObjectType"`
-	ObjectProperties       []string               `json:"objectProperties"`
-	PublicCrmSearchRequest CrmExportSearchRequest `json:"publicCrmSearchRequest"`
+	ExportType             string                 `json:"exportType,omitempty"`
+	Format                 string                 `json:"format,omitempty"`
+	ExportName             string                 `json:"exportName,omitempty"`
+	Language               string                 `json:"language,omitempty"`
+	ObjectType             string                 `json:"objectType,omitempty"`
+	AssociatedObjectType   string                 `json:"associatedObjectType,omitempty"`
+	ObjectProperties       []string               `json:"objectProperties,omitempty"`
+	PublicCrmSearchRequest CrmExportSearchRequest `json:"publicCrmSearchRequest,omitempty"`
+	ListId                 string                 `json:"listId,omitempty"`
 }
 
 type CrmExportFilter struct {
-	PropertyName string `json:"propertyName"`
-	Operator     string `json:"operator"`
-	Value        string `json:"value"`
+	PropertyName string `json:"propertyName,omitempty"`
+	Operator     string `json:"operator,omitempty"`
+	Value        string `json:"value,omitempty"`
 }
 type CrmExportSort struct {
-	PropertyName string `json:"propertyName"`
-	Order        string `json:"order"`
+	PropertyName string `json:"propertyName,omitempty"`
+	Order        string `json:"order,omitempty"`
 }
 type CrmExportSearchRequest struct {
-	Filters []CrmExportFilter
-	Query   string `json:"query"`
-	Sorts   []CrmExportSort
+	Filters []CrmExportFilter `json:"filters,omitempty"`
+	Query   string            `json:"query,omitempty"`
+	Sorts   []CrmExportSort   `json:"sorts,omitempty"`
 }
 
 func (s *CrmExportsServiceOp) Start(exportRequest *CrmExportConfig) (interface{}, error) {
