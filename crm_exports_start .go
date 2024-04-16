2314723cb04c6f9a2f2c8567fb321457286ed0b1
Wojciech Sokołowski <sokolwojtek1@gmail.com>
2024-04-16 16:59:36 +0200

    fix start

--- crm_exports_start .go
+++ crm_exports_start .go
@@ -1,103 +1,36 @@
 package hubspot
 
-import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"mime/multipart"
-	"net/textproto"
-)
-
 type CrmExportConfig struct {
-	Name                    string                `json:"name"`
-	MarketableContactExport bool                  `json:"marketableContactExport"`
-	ExportOperations        map[string]string     `json:"exportOperations"`
-	Files                   []CrmExportFileConfig `json:"files"`
+	ExportType             string                 `json:"exportType"`
+	Format                 string                 `json:"format"`
+	ExportName             string                 `json:"exportName"`
+	Language               string                 `json:"language"`
+	ObjectType             string                 `json:"objectType"`
+	AssociatedObjectType   string                 `json:"associatedObjectType"`
+	ObjectProperties       []string               `json:"objectProperties"`
+	PublicCrmSearchRequest CrmExportSearchRequest `json:"publicCrmSearchRequest"`
 }
 
-type CrmExportFilePageConfig struct {
-	HasHeader      bool                     `json:"hasHeader"`
-	ColumnMappings []CrmExportColumnMapping `json:"columnMappings"`
+type CrmExportFilter struct {
+	PropertyName string `json:"propertyName"`
+	Operator     string `json:"operator"`
+	Value        string `json:"value"`
 }
-
-type CrmExportFileConfig struct {
-	FileName       string                  `json:"fileName"`
-	FileFormat     string                  `json:"fileFormat"`
-	DateFormat     string                  `json:"dateFormat"`
-	FileExportPage CrmExportFilePageConfig `json:"fileExportPage"`
-	// Data is the CSV or Spreadsheet data for this file.
-	Data io.Reader `json:"-"`
+type CrmExportSort struct {
+	PropertyName string `json:"propertyName"`
+	Order        string `json:"order"`
 }
-
-type CrmExportColumnMapping struct {
-	ColumnObjectTypeId string `json:"columnObjectTypeId"`
-	ColumnName         string `json:"columnName"`
-	PropertyName       string `json:"propertyName"`
-	IdColumnType       string `json:"idColumnType,omitempty"`
+type CrmExportSearchRequest struct {
+	Filters []CrmExportFilter
+	Query   string `json:"query"`
+	Sorts   []CrmExportSort
 }
 
 func (s *CrmExportsServiceOp) Start(exportRequest *CrmExportConfig) (interface{}, error) {
 	resource := make(map[string]interface{})
-
-	// Body is our final result that we pass to postMultipart
-	body := &bytes.Buffer{}
-
-	// Write the exportRequest to the multipart message
-	writer := multipart.NewWriter(body)
-
-	// Write a part for the JSON metadata.
-	if err := addJSONtoExportMultipart(writer, exportRequest); err != nil {
-		return nil, err
-	}
-
-	// Write file data to multipart.
-	if err := addFilesToExportMultipart(writer, exportRequest); err != nil {
-		return nil, err
-	}
-
-	if err := writer.Close(); err != nil {
-		return nil, err
-	}
-
-	if err := s.client.PostMultipart(s.crmExportsPath, writer.Boundary(), body.Bytes(), &resource); err != nil {
+	err := s.client.Post(s.crmExportsPath, exportRequest, &resource)
+	if err != nil {
 		return nil, err
 	}
 	return resource, nil
-}
-
-func addJSONtoExportMultipart(writer *multipart.Writer, exportRequest *CrmExportConfig) error {
-	data, err := json.Marshal(exportRequest)
-	if err != nil {
-		return err
-	}
-	header := textproto.MIMEHeader{}
-	header.Set("Content-Disposition", "form-data; name=\"exportRequest\"")
-	part, err := writer.CreatePart(header)
-	if err != nil {
-		return err
-	}
-	if _, err := part.Write(data); err != nil {
-		return err
-	}
-	return nil
-}
-
-func addFilesToExportMultipart(writer *multipart.Writer, exportRequest *CrmExportConfig) error {
-	for _, fileDef := range exportRequest.Files {
-		csvHeader := textproto.MIMEHeader{}
-		csvHeader.Set("Content-Disposition", fmt.Sprintf("form-data; name=\"files\"; filename=\"%s\"", fileDef.FileName))
-		csvPart, err := writer.CreatePart(csvHeader)
-		if err != nil {
-			return err
-		}
-		fileData, err := io.ReadAll(fileDef.Data)
-		if err != nil {
-			return err
-		}
-		if _, err := csvPart.Write(fileData); err != nil {
-			return err
-		}
-	}
-	return nil
 }
